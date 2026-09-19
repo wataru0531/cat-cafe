cb4cc3d5dfd49df3c5376b74eb07abaaf8359b7c
wataru0531 <obito0531@gmail.com>
2026-09-19 22:03:28 +0900

    categoryテーブル作成 Seeder作成

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+// Models/Category.php
+// ✅ カテゴリーのモデル
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+}
---
+++ database/migrations/2026_09_19_215036_create_categories_table.php
@@ -0,0 +1,22 @@
+<?php
+
+// カテゴリーのマイグレーションファイル
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  
+  public function up(): void {
+    Schema::create("categories", function(Blueprint $table) {
+      $table->id();
+      $table->string("name")->comment("カテゴリ名");
+      $table->timestamps();
+    });
+  }
+
+  public function down(): void {
+    Schema::dropIfExists('categories');
+  }
+};
